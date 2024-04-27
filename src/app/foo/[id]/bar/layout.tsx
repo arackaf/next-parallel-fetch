56afcc8a39eabd27e68399626fe74ced73d3d0b4
Adam Rackis <adamrackis@hotmail.com>
2024-04-27 06:20:35 -0400

    test loading

---
+++ src/app/foo/[id]/bar/layout.tsx
@@ -0,0 +1,16 @@
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  console.log("Fetch in foo/[id]/bar Layout");
+  await new Promise((res) => setTimeout(res, 3000));
+  console.log("Fetch in foo/[id]/bar Layout Finished");
+
+  return (
+    <div>
+      <h1>Inner layout</h1>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/foo/[id]/bar/page.tsx
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import Component1 from "../../../Component1";
+import Component2 from "../../../Component2";
+import Component3 from "../../../Component3";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col p-24">
+      <h1>foo/[id]/bar</h1>
+      <div>
+        <Suspense fallback={<h1>Loading ...</h1>}>
+          <Component1 />
+          <Component2 />
+          <Component3 />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
